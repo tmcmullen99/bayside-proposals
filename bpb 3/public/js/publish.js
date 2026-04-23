bc6bc9a0d3b18dd2ae1b36b78c156e905403b3cf
tmcmullen99 <tim@mcmullen.properties>
2026-04-23 07:03:23 -0700

    Update publish.js

--- bpb 3/public/js/publish.js
+++ bpb 3/public/js/publish.js
@@ -80,6 +80,7 @@ const TRU_SCAPES_PRODUCT_GUIDE_URL = 'https://cdn.prod.website-files.com/65a1ca4
 const BAYSIDE_LOGO_URL = 'https://cdn.prod.website-files.com/65a1ca4354f63bd7376b5027/69a04f4369bc9cc20b8d2155_BaysidePavers_original%20(2)%20(1).png';
 const TIM_PHONE = '415-691-9272';
 const TIM_PHONE_HREF = '+14156919272';
+const TIM_EMAIL = 'Tim@BaysidePavers.com';
 const BUCKET = 'proposal-photos';
 
 let proposalId = null;
