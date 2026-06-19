a902f67ec9f4722525d4bbdb55c0b1013548b624
tmcmullen99 <tim@mcmullen.properties>
2026-06-19 08:51:12 -0700

    Update supabase-client.js

--- bpb 3/public/js/supabase-client.js
+++ bpb 3/public/js/supabase-client.js
@@ -36,8 +36,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 const SUPABASE_URL = 'https://gfgbypcnxkschnfsitfb.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmZ2J5cGNueGtzY2huZnNpdGZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3MTkzMTUsImV4cCI6MjA5MjI5NTMxNX0.EAwmiNR5OWcaI8Sr36MVn7FuMhYoZvfngse7y0ZOgvA';
-
+const SUPABASE_ANON_KEY = 'sb_publishable_Ko4LNw2VjhZVFmwJ-i2qtA_XeGgBcew';
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     persistSession: true,
